2241851216bc0625615201b91c825f1c93a0b399
松永菜月 <dicaiyue856@gmail.com>
2025-11-02 05:31:09 +0900

    Javascript記述しました

--- src/resources/views/user/attendance-register.blade.php
+++ src/resources/views/user/attendance-register.blade.php
@@ -33,3 +33,14 @@
     </form>
 </div>
 
+<script>
+    function updateTime() {
+        const now = new Date();
+        const options = { hour: '2-digit', minute: '2-digit', hour12: false };
+        document.getElementById('currentTime').value = now.
+        toLocaleTimeString('ja-JP', options);
+    }
+    setInterval(updateTime, 1000);
+    updateTime();
+</script>
+@endsection
